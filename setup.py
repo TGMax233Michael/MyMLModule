8eac52cfa36e97ea2992ea6637c95deac402adaa
TGMax233_Michael <tgmax233michael@163.com>
2025-03-28 22:07:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myMLModule',
-    version='0.0.1b',
+    version='0.0.2',
     description='自己制作的机器学习库',
     author='TGMax233_Michael',
     author_email='tgmax233michael@163.com',
